4edc77f7752a36d10c3e1c39b2af40e4ecfc5c63
David Lopez <sscampusmty@gmail.com>
2025-03-31 20:10:54 -0600

    R3emove dashboard page

--- src/app/Pages.tsx
+++ src/app/Pages.tsx
@@ -1,13 +1,11 @@
 import Proyectos from "./Proyectos/Proyectos";
 import Llamadas from "./Llamadas/Llamadas";
-import Dashboard from "./Dashboard/Dashboard";
 import Personas from "./Personas/Personas";
 import Tutoriales from "./Tutoriales/Tutoriales";
 
 export {
     Proyectos,
     Llamadas,
-    Dashboard,
     Personas,
     Tutoriales
 };
