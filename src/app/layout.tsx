0860e9ea38a821ca04e977c5a990d322056b6b68
AndresDlg562 <andy562.dlg@gmail.com>
2025-04-02 20:43:48 -0600

    made svg the default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="/icon0.svg"/>
+        <link rel="alternate icon" sizes="16x16" href="/favicon.ico"/>
+      </head>
       <body>
         {children}
       </body>
