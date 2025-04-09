1fc12cb7761109c32c09ee1fd8ed03e0299f8595
David Lopez <sscampusmty@gmail.com>
2025-04-09 10:48:13 -0600

    Fix redirect URL usage in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const CLIENT_ID = process.env.CLIENT_ID;
-const REDIRECT_URL = process.env.REDIRECT_URL;
 const TENANT = "common";
 const SCOPE = "openid profile User.Read"; 
 
@@ -9,13 +8,13 @@ const nextConfig: NextConfig = {
   env: {
     CLIENT_ID,
     OBJECT_ID: process.env.OBJECT_ID,
-    REDIRECT_URL
+    REDIRECT_URL: process.env.REDIRECT_URL,
   },
   async redirects() {
     return [
       {
         source: '/',
-        destination: `https://login.microsoftonline.com/${TENANT}/oauth2/v2.0/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URL}&response_mode=query&scope=${SCOPE}`,
+        destination: `https://login.microsoftonline.com/${TENANT}/oauth2/v2.0/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=$https://relations-web.vercel.app/login&response_mode=query&scope=${SCOPE}`,
         permanent: false,
         basePath: false
       }
